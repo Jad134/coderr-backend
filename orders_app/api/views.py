e308fa27a4d29a3791226a27bf3c4800a2b9ceef
unknown <jadelnader99@gmail.com>
2024-12-03 18:49:16 +0100

    post order

--- orders_app/api/views.py
+++ orders_app/api/views.py
@@ -9,13 +9,29 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class OrderViewSet(viewsets.ViewSet):
-    queryset = Order.objects.all()
 
+    def get_queryset(self):
+        return Order.objects.all()
 
     def list(self, request):
         """
         Get order list with queryset for filter methods in Frontend Code e.g only show own offers with creator_id
         """
-        queryset = self.queryset 
+        queryset = self.get_queryset()
         serializer = OrderSerializer(queryset, many=True)
         return Response(serializer.data)
+    
+
+    def create(self, request):
+        """
+        Create a new order.
+        """
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            # Speichere das neue Order-Objekt
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            # Falls ungültig, sende die Validierungsfehler zurück
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
