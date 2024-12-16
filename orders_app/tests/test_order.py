384510057f46f382690da8dd982bc1071a209605
unknown <jadelnader99@gmail.com>
2024-12-16 18:59:35 +0100

    order test SetUp

--- orders_app/tests/test_order.py
+++ orders_app/tests/test_order.py
@@ -3,3 +3,13 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
+
+
+class OrderViewSetTest(APITestCase):
+    def setUp(self):
+         self.user_model = get_user_model()
+         self.user1 = self.user_model.objects.create_user(username="user1", password="password123")
+         self.token = Token.objects.create(user=self.user1)
+         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
+
+         self.order_url = reverse("orders.list-list")
